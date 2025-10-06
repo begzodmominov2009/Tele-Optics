0570c875c7154da1c710910b83fd8fe397e6fbf0
begzodmominov2009 <begzodmominov32@gmail.com>
2025-10-06 11:20:16 +0300

    adding search

--- Js/main.js
+++ Js/main.js
@@ -3,14 +3,41 @@ let mini_search = document.getElementById("mini-search")
 let close = document.getElementById("close")
 
 
-Search.addEventListener("click" , () => {
+Search.addEventListener("click", () => {
     mini_search.classList.add("mini_search_hide")
 })
 
-close.addEventListener("click" , () => {
+close.addEventListener("click", () => {
     mini_search.classList.remove("mini_search_hide")
     mini_search.classList.add("close-durition")
 })
-window.addEventListener("scroll" , () => {
+window.addEventListener("scroll", () => {
     mini_search.classList.remove("mini_search_hide")
-} )
+    mini_search.classList.add("close-durition")
+
+})
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+    },
+});
